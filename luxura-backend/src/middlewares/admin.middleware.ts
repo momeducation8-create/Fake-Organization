d82ce0a349d21b175b4279d85c972f3625d9dbac
momeducation8-create <momeducation8@gmail.com>
2026-06-20 03:03:44 +0300

    Update admin.middleware.ts

--- luxura-backend/src/middlewares/admin.middleware.ts
+++ luxura-backend/src/middlewares/admin.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { AppError } from "./error.middleware";
+import { AppError } from "./error.middleware.js";
 
 /**
  * Must be chained AFTER protect middleware.
